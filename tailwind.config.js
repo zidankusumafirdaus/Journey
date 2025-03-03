a72b841f79194d4be262321d75e83b433906b2df
M Zidan Kusuma Firdaus <zidankusumafirdaus.school@gmail.com>
2025-03-03 18:58:59 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#222222",
+        background2: "#121212",
+      },
+    },
+  },
+  plugins: [],
+};
